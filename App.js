809906eb5843837dfdbbf0c77eda01fd9cdb153d
Ganna05 <ganna050779@mail.ru>
2021-10-17 11:13:36 +0300

    new file:   App.js 	new file:   erc20_.sol 	new file:   index.js

---
+++ App.js
@@ -0,0 +1,430 @@
+import React, { useState } from "react";
+//import "./styles.css";
+
+var Web3 = require("web3");
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+var abi = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address"
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "spender",
+        type: "address"
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256"
+      }
+    ],
+    name: "Approval",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address"
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address"
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256"
+      }
+    ],
+    name: "Transfer",
+    type: "event"
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address"
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256"
+      }
+    ],
+    name: "approve",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool"
+      }
+    ],
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address"
+      },
+      {
+        internalType: "uint256",
+        name: "subtractedValue",
+        type: "uint256"
+      }
+    ],
+    name: "decreaseAllowance",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool"
+      }
+    ],
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address"
+      },
+      {
+        internalType: "uint256",
+        name: "addedValue",
+        type: "uint256"
+      }
+    ],
+    name: "increaseAllowance",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool"
+      }
+    ],
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "recipient",
+        type: "address"
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256"
+      }
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool"
+      }
+    ],
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "sender",
+        type: "address"
+      },
+      {
+        internalType: "address",
+        name: "recipient",
+        type: "address"
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256"
+      }
+    ],
+    name: "transferFrom",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool"
+      }
+    ],
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "initialSupply",
+        type: "uint256"
+      },
+      {
+        internalType: "string",
+        name: "tokenName",
+        type: "string"
+      },
+      {
+        internalType: "string",
+        name: "tokenSymbol",
+        type: "string"
+      }
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor"
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address"
+      },
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address"
+      }
+    ],
+    name: "allowance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address"
+      }
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  }
+];
+
+export default function App() {
+  const [show_balance, set_balance] = useState(0);
+  const [show_name, set_name] = useState(0);
+  const [show_symbol, set_symbol] = useState(0);
+  const [show_receipt, set_receipt] = useState("");
+  const [send_amount, set_send_amount] = useState(0);
+  const [recipient, set_recipient] = useState(0);
+  const [token_address, set_token_address] = useState(0);
+  const [show_supply, set_supply] = useState(0);
+
+  const handleAmountChange = (e) => {
+    set_send_amount(e.target.value);
+  };
+
+  const handleRecipientChange = (e) => {
+    set_recipient(e.target.value);
+  };
+
+  const handleTokenAddressChange = (e) => {
+    set_token_address(e.target.value);
+  };
+
+  function getBalance() {
+    console.log("Checking  balance");
+    web3.eth
+      .getBalance("0x8A984BA6386ea53Dd988f2BFd2Fff83EB1188D92")
+      .then((balance) => {
+        console.log(
+          "Account 0x8A984BA6386ea53Dd988f2BFd2Fff83EB1188D92 balance",
+          balance
+        );
+        set_balance(Web3.utils.fromWei(balance, "ether"));
+      });
+  }
+
+  function getName() {
+    const contract = new web3.eth.Contract(abi, token_address);
+    contract.methods
+      .name()
+      .call({ from: "0x8A984BA6386ea53Dd988f2BFd2Fff83EB1188D92" })
+      .then((result) => set_name(result));
+  }
+
+  function getSymbol() {
+    const contract = new web3.eth.Contract(abi, token_address);
+    contract.methods
+      .symbol()
+      .call({ from: "0x8A984BA6386ea53Dd988f2BFd2Fff83EB1188D92" })
+      .then((result) => set_symbol(result));
+  }
+
+  function getSupply() {
+    const contract = new web3.eth.Contract(abi, token_address);
+    contract.methods
+      .totalSupply()
+      .call({ from: "0x8A984BA6386ea53Dd988f2BFd2Fff83EB1188D92" })
+      .then((result) => set_supply(result));
+  }
+
+  async function sendTokens() {
+    set_receipt(`Please wait for the transaction hash`);
+    // Sender Address!!!!
+    var account = "0x8A984BA6386ea53Dd988f2BFd2Fff83EB1188D92";
+    var privateKey = "PRIVATE_KEY";
+    const contract = new web3.eth.Contract(abi, token_address);
+    let gas = await contract.methods
+      .transfer(recipient, send_amount)
+      .estimateGas({ from: "0x8A984BA6386ea53Dd988f2BFd2Fff83EB1188D92" });
+    console.log("Esimated gas", gas);
+    const setNumbertTx = contract.methods.transfer(recipient, send_amount);
+    const createTransaction = await web3.eth.accounts.signTransaction(
+      {
+        from: account,
+        to: token_address,
+        data: setNumbertTx.encodeABI(),
+        gas: gas
+      },
+      privateKey
+    );
+    const createReceipt = await web3.eth.sendSignedTransaction(
+      createTransaction.rawTransaction
+    );
+    console.log(`Tx successfull with hash: ${createReceipt.transactionHash}`);
+    set_receipt(`Tx successfull with hash: ${createReceipt.transactionHash}`);
+  }
+
+  return (
+    <div className="App">
+      <h1>Send ERC20 tokens</h1>
+      <div>
+        <button onClick={getBalance}>Check balance</button>
+        <h4>{show_balance}</h4>
+      </div>
+
+      <input
+        placeholder="enter ERC20 address"
+        onChange={(e) => handleTokenAddressChange(e)}
+      ></input>
+      <div>
+        <button onClick={getName}>Get name</button>
+        <h4>{show_name}</h4>
+      </div>
+
+      <div>
+        <button onClick={getSymbol}>Get symbol</button>
+        <h4>{show_symbol}</h4>
+      </div>
+
+      <div>
+        <button onClick={getSupply}>Get supply</button>
+        <h4>{show_supply}</h4>
+      </div>
+
+      <div>
+        <input
+          placeholder="enter recipient"
+          onChange={(e) => handleRecipientChange(e)}
+        ></input>
+        <br />
+        <input
+          placeholder="enter number of tokens"
+          onChange={(e) => handleAmountChange(e)}
+        ></input>
+        <br />
+        <button onClick={sendTokens}>Send tokens to address</button>
+        <h4>{show_receipt}</h4>
+      </div>
+    </div>
+  );
+}
---
+++ erc20_.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract YOUR_TOKEN is ERC20 {
+    constructor(uint256 initialSupply, string memory tokenName, string memory tokenSymbol) ERC20(tokenName, tokenSymbol) {
+        _mint(msg.sender, initialSupply);
+    }
+    function decimals() public view virtual override returns (uint8) {
+        return 0;
+    }
+}
